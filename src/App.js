29a6937a903f17171efdab11f1a3d5e08afbdf64
Wili0710 <66407599+wili0710@users.noreply.github.com>
2021-01-27 10:20:22 +0700

    Basic app component

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return <div/>
+  return (
+    <div data-test="component-app">
+      
+    </div>
+  )
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import { shallow } from 'enzyme'
+import { findByTestAttr } from './../test/testUtils'
 import App from './App';
 
-test('future test goes here', () => {
-  // just a placeholder test so that jest won't complain about
-  // test-less test file
+/**
+ * Setup function for app component
+ * @returns {shallowwrapper}
+ */
+const setup = () => {
+  return shallow(<App />)
+}
+
+test('App renders without error', () => {
+  const wrapper = setup()
+  const component = findByTestAttr(wrapper, 'component-app')
+  expect(component.length).toBe(1)
 });
